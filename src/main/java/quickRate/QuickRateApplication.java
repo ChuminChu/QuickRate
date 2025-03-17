6ae4597de9fbbb4ca62361797ffa87a4fac6cf84
chumin-yeong <chu35my@gmail.com>
2025-03-17 15:12:52 +0900

    initial commit

---
+++ src/main/java/quickRate/QuickRateApplication.java
@@ -0,0 +1,13 @@
+package quickRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quickRate/QuickRateApplicationTests.java
@@ -0,0 +1,13 @@
+package quickRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
